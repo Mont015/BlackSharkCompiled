0b01ca8a4f19eae38a122b144be7b2e3ba866346
Mont <133494114+Mont015@users.noreply.github.com>
2026-09-20 17:46:56 +0200

    Refactor loading and saving logic in finishLoading function

--- main.lua
+++ main.lua
@@ -59,11 +59,12 @@ local function finishLoading()
 	vape.Init = nil
 	vape:Load()
 	task.spawn(function()
-		repeat
-			vape:Save()
-			task.wait(10)
-		until not vape.Loaded
-	end)
+    task.wait(5)
+    repeat
+        vape:Save()
+        task.wait(10)
+    until not vape.Loaded
+end)
 
 	local teleportedServers
 	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
