607a1e182969e5d7ce6157e6685d46f41f17b6a7
Mont <diamantboshnjaku9@gmail.com>
2026-09-25 15:40:41 +0200

    Refine combat timing and Blink support handling

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -2669,6 +2669,7 @@ run(function()
 	local AttackRemote
 	local LastManualSwing = 0
 	local NextAttack = 0
+	local HitInterval = 2 / 7
 	local AttackIndex = 1
 	local PrimaryTarget
 	local AnimationToken = 0
@@ -2953,7 +2954,7 @@ run(function()
 				end
 
 				repeat
-					local now = tick()
+					local now = workspace:GetServerTimeNow()
 					local sword, meta = getAttackData()
 					local root = entitylib.character and entitylib.character.RootPart
 					local targets = sword and root and collectTargets(root) or {}
@@ -2979,9 +2980,9 @@ run(function()
 							local sent = attackTarget(sword, root, target)
 							if sent then
 								AttackIndex = (targetIndex % #attackable) + 1
-								NextAttack = tick() + math.max(tonumber(meta.sword.attackSpeed) or 0.11, 0.05)
+								NextAttack = now + HitInterval
 							else
-								NextAttack = tick() + 0.1
+								NextAttack = now + 0.1
 							end
 						end
 					else
--- games/universal.lua
+++ games/universal.lua
@@ -6486,15 +6486,29 @@ run(function()
 	local AutoSend
 	local AutoSendLength
 	local oldphys, oldsend
+
+	local function setNetworkRates(physicsrate, senderrate)
+		if type(setfflag) ~= 'function' then return false end
+		return pcall(function()
+			setfflag('PhysicsSenderMaxBandwidthBps', physicsrate)
+			setfflag('DataSenderRate', senderrate)
+		end)
+	end
 	
 	Blink = vape.Categories.Utility:CreateModule({
 		Name = 'Blink',
 		Function = function(callback)
 			if callback then
+				if type(setfflag) ~= 'function' then
+					notif('Blink', 'Fake lag is not supported by this executor.', 5, 'warning')
+					task.defer(function()
+						if Blink.Enabled then Blink:Toggle() end
+					end)
+					return
+				end
 				local teleported
 				Blink:Clean(lplr.OnTeleport:Connect(function()
-					setfflag('PhysicsSenderMaxBandwidthBps', '38760')
-					setfflag('DataSenderRate', '60')
+					setNetworkRates('38760', '60')
 					teleported = true
 				end))
 	
@@ -6505,22 +6519,24 @@ run(function()
 					end
 	
 					if physicsrate ~= oldphys or senderrate ~= oldsend then
-						setfflag('PhysicsSenderMaxBandwidthBps', physicsrate)
-						setfflag('DataSenderRate', senderrate)
+						if not setNetworkRates(physicsrate, senderrate) then
+							notif('Blink', 'Fake lag is not supported by this executor.', 5, 'warning')
+							task.defer(function()
+								if Blink.Enabled then Blink:Toggle() end
+							end)
+							break
+						end
 						oldphys, oldsend = physicsrate, senderrate
 					end
 	
 					task.wait(0.03)
 				until (not Blink.Enabled and not teleported)
 			else
-				if setfflag then
-					setfflag('PhysicsSenderMaxBandwidthBps', '38760')
-					setfflag('DataSenderRate', '60')
-				end
+				setNetworkRates('38760', '60')
 				oldphys, oldsend = nil, nil
 			end
 		end,
-		Tooltip = 'Chokes packets until disabled.'
+		Tooltip = 'Chokes packets until disabled. Requires executor support.'
 	})
 	Type = Blink:CreateDropdown({
 		Name = 'Type',
